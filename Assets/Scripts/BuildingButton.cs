ac2d69a087d691026d998596fa79391a3bc71420
Sergey <sergeytrost@gmail.com>
2019-11-10 02:17:20 +0500

    Building

---
+++ Assets/Scripts/BuildingButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingButton : MonoBehaviour
+{
+    public GameObject Prefab;
+    public GameObject Terrain;
+    private GameObject building;
+    private bool isBuilt;
+    public void OnClick()
+    {
+        building = Instantiate(Prefab);
+        isBuilt = false;
+        StartCoroutine(BuildingPositionUpdate());
+    }
+    IEnumerator BuildingPositionUpdate()
+    {
+        while (!isBuilt)
+        {
+            RaycastHit[] hits;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            hits = Physics.RaycastAll(ray);
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.collider != null && hit.collider.gameObject == Terrain)
+                {
+                    Vector3 offset = new Vector3(0, building.GetComponent<Renderer>().bounds.size.y / 2 - 0.04f, 0);
+                    building.transform.position = hit.point + offset;
+                    break;
+                }
+            }
+            BuildingView buildingView = building.GetComponent<BuildingView>();
+            if (Input.GetMouseButtonDown(0) && !buildingView.IsCollision)
+            {
+                isBuilt = true;
+                buildingView.IsBuilt = isBuilt;
+                buildingView.OnBuild.Invoke();
+            }
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/BuildingView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BuildingView : MonoBehaviour
+{
+    public bool IsCollision { get; private set; }
+    public UnityEvent OnCollision = new UnityEvent();
+    public UnityEvent OnRelease = new UnityEvent();
+    public UnityEvent OnBuild = new UnityEvent();
+    public bool IsBuilt;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!IsBuilt)
+        {
+            if (!IsCollision)
+                OnCollision.Invoke();
+            IsCollision = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (!IsBuilt)
+        {
+            OnRelease.Invoke();
+            IsCollision = false;
+        }
+    }
+}
