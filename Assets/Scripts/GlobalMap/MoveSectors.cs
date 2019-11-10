b238bfea7600b647d8c4a006ef840fccce20e70b
buzzon <buzzondev@gmail.com>
2019-11-10 20:51:22 +0500

    fix texture

--- Assets/Scripts/GlobalMap/MoveSectors.cs
+++ Assets/Scripts/GlobalMap/MoveSectors.cs
@@ -8,39 +8,34 @@ public class MoveSectors : MonoBehaviour
     [SerializeField] private BoxCollider _world;
     [SerializeField] private GameObject _sector;
     [SerializeField] private int _sectorsInLine;
-    [SerializeField] private float _distanceBetweenSectors;
+    [SerializeField] private float _offset;
     [SerializeField] private float _moveSpeed;
-    private Vector3 _offset;
+    private List<GameObject> _sectors;
 
-    private List<GameObject> _children;
+    void Awake()
+    {
+        _sectors = new List<GameObject>(_sectorsInLine * _sectorsInLine);
+    }
 
     void Start()
     {
-        _children = new List<GameObject>(_sectorsInLine * _sectorsInLine);
-
-        _offset = new Vector3(_sector.GetComponent<Renderer>().bounds.size.x + _distanceBetweenSectors,
-                              _sector.GetComponent<Renderer>().bounds.size.y + _distanceBetweenSectors,
-                              _sector.GetComponent<Renderer>().bounds.size.z + _distanceBetweenSectors);
-
         CreateMap();
     }
 
     void FixedUpdate()
     {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
-        if (!(Math.Abs(horizontal) > 0.1f) && !(Math.Abs(vertical) > 0.1f)) return;
+        if (Math.Abs(horizontal) < 0.01f && Math.Abs(vertical) < 0.01f) return;
+
         transform.position += new Vector3(horizontal, 0, vertical) * Time.deltaTime * _moveSpeed;
 
-        foreach (var sector in _children)
+        foreach (GameObject sector in _sectors)
         {
-            var position = sector.transform.position;
+            Vector3 position = sector.transform.position;
 
-            if (position.x < _world.bounds.min.x)
-                sector.transform.position = new Vector3(_world.bounds.max.x + position.x - _world.bounds.min.x, position.y, position.z);
-            else if (position.x > _world.bounds.max.x)
-                sector.transform.position = new Vector3(_world.bounds.min.x + position.x - _world.bounds.max.x, position.y, position.z);
+            NewMethod(position, sector);
 
             if (position.z < _world.bounds.min.z)
                 sector.transform.position = new Vector3(position.x, position.y, _world.bounds.max.z + position.z - _world.bounds.min.z);
@@ -49,16 +44,37 @@ public class MoveSectors : MonoBehaviour
         }
     }
 
+    private void NewMethod(Vector3 position, GameObject sector)
+    {
+        if (position.x < _world.bounds.min.x)
+            sector.transform.position =
+                new Vector3(_world.bounds.max.x + position.x - _world.bounds.min.x, position.y, position.z);
+        else if (position.x > _world.bounds.max.x)
+            sector.transform.position =
+                new Vector3(_world.bounds.min.x + position.x - _world.bounds.max.x, position.y, position.z);
+    }
+
     private void CreateMap()
     {
+        Vector3 sectorSize = _sector.GetComponent<Renderer>().bounds.size;
+        Vector3 sectorOffset = new Vector3(sectorSize.x + _offset, sectorSize.y + _offset, sectorSize.z + _offset);
+
         for (int j = 0; j < _sectorsInLine; j++)
+        {
             for (int i = 0; i < _sectorsInLine; i++)
             {
-                var sector = Instantiate(_sector, new Vector3(i * _offset.x + (_offset.x / 2) * (j % 2), 0, j * _offset.z * 3 / 4), Quaternion.identity, transform);
-                _children.Add(sector);
+                Vector3 position = new Vector3(i * sectorOffset.x + (sectorOffset.x / 2) * (j % 2), 0, j * sectorOffset.z * 3 / 4);
+                GameObject sector = Instantiate(_sector, position, Quaternion.identity, transform);
+                _sectors.Add(sector);
             }
+        }
 
-        _world.size = new Vector3(_offset.x * _sectorsInLine, 3, _offset.z * 3 / 4 * _sectorsInLine);
+        SetBoundMap(sectorOffset);
+    }
+
+    private void SetBoundMap(Vector3 sectorOffset)
+    {
+        _world.size = new Vector3(sectorOffset.x * _sectorsInLine, 3, sectorOffset.z * 3 / 4 * _sectorsInLine);
         _world.center = new Vector3(_world.size.x / 2, 0, _world.size.z / 2);
     }
 }
