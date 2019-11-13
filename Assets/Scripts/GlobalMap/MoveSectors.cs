ab2aaac8d118eeaaf919e32d4f9cb84b1c13a64b
buzzon <buzzondev@gmail.com>
2019-11-13 19:50:34 +0500

    минирефакторинг MoveSectors

--- Assets/Scripts/GlobalMap/MoveSectors.cs
+++ Assets/Scripts/GlobalMap/MoveSectors.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveSectors : MonoBehaviour
 {
     [SerializeField] private BoxCollider _world;
-    [SerializeField] private GameObject _sector;
+    [SerializeField] private GameObject _mesh;
     [SerializeField] private int _sectorsInLine;
     [SerializeField] private float _offset;
     [SerializeField] private float _moveSpeed;
@@ -19,57 +18,54 @@ public class MoveSectors : MonoBehaviour
 
     void Start()
     {
-        CreateMap();
+        Vector3 sectorSize = _mesh.GetComponent<Renderer>().bounds.size;
+        Vector3 fullSectorSize = new Vector3(sectorSize.x + _offset, sectorSize.y + _offset, sectorSize.z + _offset);
+
+        CreateMap(fullSectorSize);
+        SetBoundMap(fullSectorSize);
     }
 
     void FixedUpdate()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (!IsInputReceived(out float horizontal, out float vertical)) return;
 
-        if (Math.Abs(horizontal) < 0.01f && Math.Abs(vertical) < 0.01f) return;
-
-        transform.position += new Vector3(horizontal, 0, vertical) * Time.deltaTime * _moveSpeed;
+        transform.position -= new Vector3(horizontal, 0, vertical).normalized * Time.deltaTime * _moveSpeed;
 
         foreach (GameObject sector in _sectors)
         {
-            Vector3 position = sector.transform.position;
+            Vector3 pos = sector.transform.position;
 
-            NewMethod(position, sector);
+            if (pos.x < _world.bounds.min.x)
+                sector.transform.position = new Vector3(_world.bounds.max.x + pos.x - _world.bounds.min.x, pos.y, pos.z);
+            else if (pos.x > _world.bounds.max.x)
+                sector.transform.position =  new Vector3(_world.bounds.min.x + pos.x - _world.bounds.max.x, pos.y, pos.z);
 
-            if (position.z < _world.bounds.min.z)
-                sector.transform.position = new Vector3(position.x, position.y, _world.bounds.max.z + position.z - _world.bounds.min.z);
-            else if (position.z > _world.bounds.max.z)
-                sector.transform.position = new Vector3(position.x, position.y, _world.bounds.min.z + position.z - _world.bounds.max.z);
+            if (pos.z < _world.bounds.min.z)
+                sector.transform.position = new Vector3(pos.x, pos.y, _world.bounds.max.z + pos.z - _world.bounds.min.z);
+            else if (pos.z > _world.bounds.max.z)
+                sector.transform.position = new Vector3(pos.x, pos.y, _world.bounds.min.z + pos.z - _world.bounds.max.z);
         }
     }
 
-    private void NewMethod(Vector3 position, GameObject sector)
+    private static bool IsInputReceived(out float horizontal, out float vertical)
     {
-        if (position.x < _world.bounds.min.x)
-            sector.transform.position =
-                new Vector3(_world.bounds.max.x + position.x - _world.bounds.min.x, position.y, position.z);
-        else if (position.x > _world.bounds.max.x)
-            sector.transform.position =
-                new Vector3(_world.bounds.min.x + position.x - _world.bounds.max.x, position.y, position.z);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        return Math.Abs(horizontal) > 0.01f || Math.Abs(vertical) > 0.01f;
     }
 
-    private void CreateMap()
+    private void CreateMap(Vector3 sectorOffset)
     {
-        Vector3 sectorSize = _sector.GetComponent<Renderer>().bounds.size;
-        Vector3 sectorOffset = new Vector3(sectorSize.x + _offset, sectorSize.y + _offset, sectorSize.z + _offset);
-
         for (int j = 0; j < _sectorsInLine; j++)
         {
             for (int i = 0; i < _sectorsInLine; i++)
             {
                 Vector3 position = new Vector3(i * sectorOffset.x + (sectorOffset.x / 2) * (j % 2), 0, j * sectorOffset.z * 3 / 4);
-                GameObject sector = Instantiate(_sector, position, Quaternion.identity, transform);
+                GameObject sector = Instantiate(_mesh, position, Quaternion.identity, transform);
                 _sectors.Add(sector);
             }
         }
-
-        SetBoundMap(sectorOffset);
     }
 
     private void SetBoundMap(Vector3 sectorOffset)
